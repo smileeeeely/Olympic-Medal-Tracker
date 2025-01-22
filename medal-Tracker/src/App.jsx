6533a87683ba25b45d273f13aec878f0cc1a09ca
smileeeeely <minjoole.ivy@gmail.com>
2025-01-22 16:53:49 +0900

    update: create new list

--- medal-Tracker/src/App.jsx
+++ medal-Tracker/src/App.jsx
@@ -1,94 +1,138 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const Header = () => {
+const App = () => {
   //여기는 js!
-  const [items, setItems] = useState({
-    contry: "",
-    goldMedal: 0,
-    silverMedal: 0,
-    bronzeMedal: 0,
-  });
+  const [contries, setContries] = useState([
+    {
+      id: new Date().getTime(),
+      contry: "cancada",
+      goldMedal: 1,
+      silverMedal: 2,
+      bronzeMedal: 3,
+    },
+    {
+      id: new Date().getTime() + 1,
+      contry: "korea",
+      goldMedal: 3,
+      silverMedal: 4,
+      bronzeMedal: 2,
+    },
+    {
+      id: new Date().getTime() + 2,
+      contry: "japan",
+      goldMedal: 0,
+      silverMedal: 1,
+      bronzeMedal: 0,
+    },
+  ]);
 
   const addContryName = (e) => {
     const contry = e.target.value;
-    setItems({ ...items, contry });
+    setContries({ ...contries, contry });
   };
 
   const addGoldMedal = (e) => {
     const goldMedal = Number(e.target.value);
-    setItems({ ...items, goldMedal });
+    setContries({ ...contries, goldMedal });
   };
 
   const addSilverMedal = (e) => {
     const silverMedal = Number(e.target.value);
-    setItems({ ...items, silverMedal });
+    setContries({ ...contries, silverMedal });
   };
 
   const addBronzeMedal = (e) => {
     const bronzeMedal = Number(e.target.value);
-    setItems({ ...items, bronzeMedal });
+    setContries({ ...contries, bronzeMedal });
   };
-  console.log(items);
+  console.log(contries);
 
   const clickEvent = (e) => {
     e.preventDefault();
-  }
+
+    const newContry = {
+      id: new Date().getTime(),
+      contry: contry,
+      goldMedal: goldMedal,
+      silverMedal: silverMedal,
+      bronzeMedal: bronzeMedal, 
+    }
+    // console.log("newContry", newContry);
+    setContries([...contries, newContry]);
+
+  };
+
+  const [contry, setContry] = useState("");
+  const [goldMedal, setGoldMedal] = useState(0);
+  const [silverMedal, setSilverMedal] = useState(0);
+  const [bronzeMedal, setBronzeMedal] = useState(0);
 
   return (
     // 여기는 html!
-    <div className="header">
-      <h1 className="title">2024 파리 올림픽</h1>
-      <form onSubmit={clickEvent} className="input-form">
-        <label>
-          국가:{" "}
-          <input
-            onChange={addContryName}
-            id="contryInput"
-            className="input-Box"
-            type="text"
-          />
-        </label>
-        <label>
-          금메달:{" "}
-          <input
-            onChange={addGoldMedal}
-            className="input-Box"
-            type="number"
-            defaultValue={0}
-          />
-        </label>
-        <label>
-          은메달:{" "}
-          <input
-            onChange={addSilverMedal}
-            className="input-Box"
-            type="number"
-            defaultValue={0}
-          />
-        </label>
-        <label>
-          동메달:{" "}
-          <input
-            onChange={addBronzeMedal}
-            className="input-Box"
-            type="number"
-            defaultValue={0}
-          />
-        </label>
-        <button className="button" id="add_button">
-          국가 추가
-        </button>
-        <button className="button" id="update_button">
-          업데이트
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="header">
+        <h1 className="title">2024 파리 올림픽</h1>
+        <form onSubmit={clickEvent} className="input-form">
+          <label>
+            국가:
+            <input
+              onChange={(e) => {setContry(e.target.value)}}
+              id="contryInput"
+              className="input-Box"
+              type="text"
+            />
+          </label>
+          <label>
+            금메달:
+            <input
+              onChange={(e) => {setGoldMedal(e.target.value)}}
+              className="input-Box"
+              type="number"
+              defaultValue={0}
+            />
+          </label>
+          <label>
+            은메달:
+            <input
+              onChange={(e) => {setSilverMedal(e.target.value)}}
+              className="input-Box"
+              type="number"
+              defaultValue={0}
+            />
+          </label>
+          <label>
+            동메달:
+            <input
+              onChange={(e) => {setBronzeMedal(e.target.value)}}
+              className="input-Box"
+              type="number"
+              defaultValue={0}
+            />
+          </label>
+
+          <button className="button" id="add_button">
+            국가 추가
+          </button>
+          <button className="button" id="update_button">
+            업데이트
+          </button>
+        </form>
+      </div>
+
+      {contries.map(function (list) {
+        return <List key={list.id} list={list}/>;
+      })}
+    </>
   );
 };
 
-const App = () => {
-  return Header();
-};
-
 export default App;
+
+const List = ({list}) => {
+  return (
+  <div className="listStyle">
+    {list.contry}  {list.goldMedal} {list.silverMedal}  {list.bronzeMedal}
+  </div>
+  );
+};
